9cba1768fa4ec5e81a5654e6fab03d1700bac87a
deeiva <divabrijitjohn@gmail.com>
2026-01-28 14:54:28 +0530

    kite

--- pgm3.java
+++ pgm3.java
@@ -9,15 +9,17 @@ public class pgm3 {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter the first real number: ");
         a1=Integer.parseInt(br.readLine());
-        System.out.println("Enter the second real numbers: ");
-        a2=Integer.parseInt(br.readLine());
         System.out.println("Enter the first imaginary number: ");
         b1=Integer.parseInt(br.readLine());
+        System.out.println("Enter the second real numbers: ");
+        a2=Integer.parseInt(br.readLine());
         System.out.println("Enter the second imaginary number: ");
         b2=Integer.parseInt(br.readLine());
         a3=a1+a2;
         b3=b1+b2;
-        System.out.println("The resultant Complex number is: "+a3+"+i"+b3);
+        System.out.println("The first Complex number is: "+a1+" + "+b1+" i");
+        System.out.println("The second Complex number is: "+a2+" + "+b2+" i");
+        System.out.println("The resultant Complex number is: "+a3+" + "+b3+" i");
 
 
     }
